448c4284cb99e014d82d414043b9ba711a4f9fc4
Faizan Muhammad <faizan.muhammad@tufts.edu>
2018-05-15 17:16:19 -0400

    Update roamer_demo.cpp

--- tbot2_examples/tbot2_roamer/src/roamer_demo.cpp
+++ tbot2_examples/tbot2_roamer/src/roamer_demo.cpp
@@ -28,7 +28,7 @@ int move_turtle_bot (double x, double y, double yaw)
         move_base_msgs::MoveBaseGoal goal;
         
         
-        std::cout<<"Target Position :"<< x  << y << std::endl;
+        std::cout<<"Target Position : "<< x  << << ", " << y << std::endl;
         
         //set the header
         goal.target_pose.header.stamp = ros::Time::now();
@@ -58,18 +58,18 @@ int main(int argc, char **argv)
         
         const int num_locations = 6;
         double locations[num_locations][3] = { {-0.432,6.3,0.0},
-					       {5.48,6.4,0.0},
-					       {21.3,6.16,0.0},
-					       {21.8,13.9,0.0},
-					       {5.8,13.9,0.0},
-					       {-0.444,13.8,0.0}};
+											   {5.48,6.4,0.0},
+											   {21.3,6.16,0.0},
+											   {21.8,13.9,0.0},
+											   {5.8,13.9,0.0},
+											   {-0.444,13.8,0.0}};
 											   
 		std::string location_names [num_locations] = {"Stairs",
-							      "Kitchen Right",
-							      "Collab Room Right",
-							      "Collab Room Left",
-							      "Kitchen Left",
-							      "Elevator"};
+												      "Kitchen Right",
+												      "Collab Room Right",
+												      "Collab Room Left",
+												      "Kitchen Left",
+												      "Elevator"};
         
         double x1,y1,x2,y2,x3,y3,x4,y4 = 0;
         
@@ -79,7 +79,7 @@ int main(int argc, char **argv)
         while (ros::ok()) {
                 
                 //move to next location
-                std::cout<<"Target Location :"<< location_names[goal_index] << std::endl;
+                std::cout<<"Target Location : "<< location_names[goal_index] << std::endl;
 
                 move_turtle_bot(locations[goal_index][0],locations[goal_index][1],locations[goal_index][2]);
                 
