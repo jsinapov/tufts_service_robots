007896dc6284257e6be10dc6165fbd8c0457f6fe
EllaBisbee <40245709+EllaBisbee@users.noreply.github.com>
2018-08-17 15:38:13 -0400

    Update poi_scan_client.py

--- tbot2_data_collection/poi_scan/scripts/poi_scan_client.py
+++ tbot2_data_collection/poi_scan/scripts/poi_scan_client.py
@@ -66,7 +66,7 @@ def scan():
 
     goal = PoiScanGoal()
     goal.topics = ['/camera/depth/image_raw', '/camera/rgb/image_raw', '/amcl_pose', '/people_tracker_measurements', 
-          '/audio', '/costmap_2d', '/scan' ]
+          '/audio', '/costmap_2d', '/scan' ] #TODO ELLA: costmap topic test it
     # goal.topics = ['odom', 'clock']  # available in gazebo
     goal.bagfile_name_prefix = 'topic_tests'
     goal.num_stops = 8
